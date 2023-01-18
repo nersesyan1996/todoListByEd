6d29be185003dcafd8c672497af8cffd8b8f79b4
Colak Nersesyan <colak81996@gmail.com>
2023-01-18 16:53:44 +0400

    edit function adding

--- script.js
+++ script.js
@@ -2,132 +2,179 @@ let input = document.querySelector(".form-control");
 let btnprimary = document.querySelector(".btn");
 let todoArray = [];
 let list_cont = document.querySelector(".list-cont");
-// let parent3 = document.querySelectorAll(".parent3");
- // let answer = document.querySelector(".todoItemvalue").contentEditable;
-let showButton = document
-  .querySelector(".showButton")
-  .addEventListener("click", () => {
-    localStorage.clear();
- 
-    list_cont.innerHTML = "";
-  });
 
-if (localStorage.getItem("testObject")) {
-  todoArray.push(...JSON.parse(localStorage.getItem("testObject")));
-  todoArray.forEach((v, i) => {
-    creattodofunc(v.value);
-
-    if (v.checked == true) {
-      let divval = document.querySelectorAll(".todoItemvalue");
-      divval[i].style.textDecoration = "line-through";
-      let checkinp = document.querySelectorAll(".checkinp");
-
-      checkinp[i].checked = true;
-    }
-  });
-}
-
-function remove(del, fromLocal) {
-  del.forEach((v, delindex) => {
-    v.addEventListener("click", () => {
-      fromLocal.forEach((valid, ind) => {
-        if (valid.checked === true && ind ==delindex) {
-              console.log(valid);
-
-          fromLocal.splice(delindex, 1);
-          list_cont.innerHTML = "";
-          fromLocal.forEach((v) => creattodofunc(v.value));
-          localStorage.setItem("testObject", JSON.stringify(fromLocal));
-        }
-      });
-    });
-  });
-}
-
-function chengevaluefunc(todoItemvalue, fromLocal) {
-      todoItemvalue.forEach((val,ind)=>{
-        console.log(parent3);
-      })
-}
+btnprimary.addEventListener("click", () => {
+  createtodoitems(input.value);
+editfunction() 
+// removeList()
+});
+btnprimary.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    createtodoitems(input.value);
+editfunction() 
+// removeList() 
+  }
+});
 
 
 function creattodofunc(input) {
+
   let parent3 = document.createElement("div");
-  parent3.classList.add("parent3");
+  parent3.classList.add("parent3"); 
 
-  parent3.innerHTML = ` <div class="sections1">
+  parent3.innerHTML = ` <div class="sections1 ">
 
-                              <h5 class='todoItemvalue' contenteditable="false">${input}</h5>
+                              <h5 class='todoItemvalue' contenteditable="false" >${input}</h5>
                               </div>
                               <div class="sections">
 
-                              <i class="bi bi-check2"></i>
+                              <i class="bi bi-check2" ></i>
 
                               <input type="checkbox" class="checkinp" >
                               
-                              <i class="bi bi-trash-fill del"></i>
+                              <i class="bi bi-trash-fill del " ></i>
+                           
                               </div> `;
   list_cont.append(parent3);
 
-  let checkinp = document.querySelectorAll(".checkinp");
-  let fromLocal = JSON.parse(localStorage.getItem("testObject"));
-  let del = document.querySelectorAll(".del");
-  let bicheck2 = document.querySelectorAll(".bi-check2");
-  let todoItemvalue = document.querySelectorAll(".todoItemvalue");
-
-  checkinp.forEach((val, index) => {
-    val.addEventListener("click", () => {
-      checkingFunk(val, index, fromLocal);
-    });
-  });
-
-  remove(del, fromLocal);
-
-  bicheck2.forEach((val, index) => {
-    val.addEventListener("click", () => {
-      chengevaluefunc(todoItemvalue, fromLocal);
-    });
-  });
-
-  
+  crossingline()
 }
 
-function checkingFunk(val, index, fromLocal) {
-  let divval = document.querySelectorAll(".todoItemvalue");
-  if (val.checked === true) {
-    fromLocal[index].checked = true;
-    divval[index].style.textDecoration = "line-through";
-    localStorage.setItem("testObject", JSON.stringify(fromLocal));
-  } else {
-    divval[index].style.textDecoration = "none";
-    fromLocal[index].checked = false;
-    localStorage.setItem("testObject", JSON.stringify(fromLocal));
-  }
-}
+function createtodoitems(){
 
-let createtodoitems = () => {
   if (input.value) {
     let todoitem = {
       value: input.value,
       checked: false,
     };
-    input.value = "";
 
+    input.value = "";
     todoArray.push(todoitem);
     localStorage.setItem("testObject", JSON.stringify(todoArray));
     let fromLocal = JSON.parse(localStorage.getItem("testObject"));
+    
     creattodofunc(fromLocal[fromLocal.length - 1].value);
-
-   
     return todoArray;
+
   }
+
 };
 
-btnprimary.addEventListener("click", () => {
-  createtodoitems();
-});
-btnprimary.addEventListener("keypress", (e) => {
-  if (e.keyCode === 13) {
-    createtodoitems();
-  }
-});
+if (localStorage.getItem("testObject")) {
+  todoArray.push(...JSON.parse(localStorage.getItem("testObject")));
+  todoArray.forEach((v, i) => {
+    creattodofunc(v.value);
+      if (v.checked == true) {
+
+        let divval = document.querySelectorAll(".todoItemvalue");
+        let checkinp = document.querySelectorAll(".checkinp");
+        divval[i].style.textDecoration = "line-through";
+        checkinp[i].checked = true;
+      }
+  });
+
+}
+ 
+function crossingline() {
+
+  let checkinp = document.querySelectorAll(".checkinp");
+  checkinp.forEach((val,ind)=>{
+    val.addEventListener("click",()=>{
+      todoArray.length = 0
+      todoArray.push(...JSON.parse(localStorage.getItem("testObject")));
+
+      let divval = document.querySelectorAll(".todoItemvalue");
+      if (val.checked==true) {
+
+        divval[ind].style.textDecoration = "line-through";
+        todoArray[ind].checked=val.checked
+        localStorage.setItem("testObject", JSON.stringify(todoArray));
+        removeList()
+
+        
+      } else {
+
+        divval[ind].style.textDecoration = "none";
+        todoArray[ind].checked=val.checked
+        localStorage.setItem("testObject", JSON.stringify(todoArray));
+        removeList()
+
+      }
+     
+    })
+   
+  })
+}
+function removeList() {
+  let del = document.querySelectorAll(".del");
+  let parent3 = document.querySelectorAll(".parent3");
+  todoArray.length = 0
+  todoArray.push(...JSON.parse(localStorage.getItem("testObject")));
+
+  del.forEach((val,ind)=>{
+    val.addEventListener("click",()=>{
+      if (todoArray[ind].checked===true) {
+        
+          parent3[ind].remove()
+          todoArray[ind]=''
+          console.log(ind);
+          let user = todoArray.filter(v => v.value != undefined);
+         
+      
+          
+          let list_cont = document.querySelector(".list-cont");
+          list_cont.innerHTML = ''
+          user.forEach((v, i) => {
+            creattodofunc(v.value);
+              if (v.checked == true) {
+        
+                let divval = document.querySelectorAll(".todoItemvalue");
+                let checkinp = document.querySelectorAll(".checkinp");
+                divval[i].style.textDecoration = "line-through";
+                checkinp[i].checked = true;
+              }
+          });
+
+
+          localStorage.setItem("testObject", JSON.stringify(user));
+      }
+    })
+   })
+
+
+//    function arrayRemove(arr, value) { 
+    
+//     return arr.filter(function(ele){ 
+//         return ele != value; 
+//     });
+// }
+
+// var result = arrayRemove(array, 6);
+
+
+}
+
+
+function editfunction() {
+  let bicheck2 = document.querySelectorAll('.bi-check2')
+  let todoItemvalue = document.querySelectorAll('.todoItemvalue')
+
+  bicheck2.forEach((v,ind)=>{
+    v.addEventListener('click',()=>{
+      if (todoItemvalue[ind].getAttribute('contenteditable')=='false') {
+        todoItemvalue[ind].setAttribute('contenteditable',true)
+
+      } else {
+        todoItemvalue[ind].setAttribute('contenteditable',false)
+
+             todoArray.length=0  
+             todoArray.push(...JSON.parse(localStorage.getItem("testObject")));
+             todoArray[ind].value=todoItemvalue[ind].textContent
+             localStorage.setItem("testObject", JSON.stringify(todoArray));
+          }
+
+    })
+  })
+}
+editfunction() 
+removeList()
